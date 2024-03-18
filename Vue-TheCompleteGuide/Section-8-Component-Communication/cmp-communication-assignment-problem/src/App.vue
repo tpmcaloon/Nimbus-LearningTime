f9686683f3a62fb8631929f1d0aebac12c555382
Tom McAloon <95248992+tpmcaloon@users.noreply.github.com>
2024-03-18 16:35:29 +0000

    Vue - The Comple Guide: Section 8 Assignment

---
+++ Vue-TheCompleteGuide/Section-8-Component-Communication/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <user-data @enterUser="showActiveUser"></user-data>
+    <active-user :username="user.name" :userAge="user.age"></active-user>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "",
+        age: "",
+      },
+    };
+  },
+  methods: {
+    showActiveUser(name, age) {
+      // console.log(typeof age);
+      this.user = {
+        name,
+        age,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:focus {
+  outline: none;
+}
+
+body {
+  color: #111;
+  background: #d5d5ff;
+  font-size: 1.4rem;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  padding: 6rem;
+  display: flex;
+  justify-content: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  max-width: 600px;
+}
+</style>
---
+++ Vue-TheCompleteGuide/Section-8-Component-Communication/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,36 @@
+<template>
+  <section v-show="username && userAge">
+    <h2 v-if="userAge">{{ username }} is {{ userAge }} Years Old</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    userAge: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+h3 {
+  text-align: center;
+  width: 100%;
+  background: yellow;
+}
+
+section {
+  display: flex;
+  width: 100%;
+  gap: 1rem;
+  padding: 1rem;
+  border: 3px dashed #7d7de0;
+}
+</style>
