d9b8b4d2d2b1c22ee1edf3902b5dd597709f81ba
Tom McAloon <95248992+tpmcaloon@users.noreply.github.com>
2024-03-18 15:31:47 +0000

    Vue-The Complete Guide: Section 7 (Done)

---
+++ Vue-TheCompleteGuide/Section-7-Vue-CLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "john",
+        name: "John Doe",
+        phone: "0123 45678 90",
+        email: "jdoe@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
