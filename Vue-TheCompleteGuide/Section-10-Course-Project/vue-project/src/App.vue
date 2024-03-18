7cfe7900606d245e1976b3c19c9f12d2d753ed6e
Tom McAloon <95248992+tpmcaloon@users.noreply.github.com>
2024-03-18 17:29:08 +0000

    Vue - The Complete Guide: Section 10 Course Project (Learning App)

--- Vue-TheCompleteGuide/Section-10-Course-Project/vue-project/src/App.vue
+++ Vue-TheCompleteGuide/Section-10-Course-Project/vue-project/src/App.vue
@@ -1,47 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import TheHeader from './components/layouts/TheHeader.vue'
+import TheResources from './components/learning-resources/TheResources.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheResources
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+html {
+  font-family: 'Roboto', sans-serif;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+body {
+  margin: 0;
 }
 </style>
