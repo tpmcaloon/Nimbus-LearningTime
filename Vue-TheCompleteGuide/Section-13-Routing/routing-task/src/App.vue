610251ab2f8c211c2b2debd8fd9d03e34d027270
Tom McAloon <95248992+tpmcaloon@users.noreply.github.com>
2024-04-10 10:40:27 +0100

    Vue The Complete Guide: Section 13 Routing (Done)

--- Vue-TheCompleteGuide/Section-13-Routing/routing-task/src/App.vue
+++ Vue-TheCompleteGuide/Section-13-Routing/routing-task/src/App.vue
@@ -1,85 +1,55 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <the-navigation></the-navigation>
+  <main>
+    <router-view></router-view>
+  </main>
+  <footer>
+    <router-view name="footer"></router-view>
+  </footer>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import TheNavigation from './components/nav/TheNavigation.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  components: {
+    TheNavigation
+  },
+  data() {
+    return {
+      teams: [
+        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
+        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
+        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
+        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
+      ],
+    };
+  },
+  provide() {
+    return {
+      teams: this.teams,
+      users: this.users,
+    };
   }
+};
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<style>
+* {
+  box-sizing: border-box;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+html {
+  font-family: sans-serif;
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+body {
+  margin: 0;
 }
 </style>
--- Vue-TheCompleteGuide/Section-13-Routing/routing-task/src/router/index.js
+++ Vue-TheCompleteGuide/Section-13-Routing/routing-task/src/router/index.js
@@ -1,23 +1,75 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+import TeamsList from "../views/TeamsList.vue";
+import UsersList from "../views/UsersList.vue";
+import TeamMembers from "../components/teams/TeamMembers.vue";
+import NotFound from "../views/NotFound.vue";
+import TeamsFooter from "../views/TeamsFooter.vue";
+import UsersFooter from "../views/UsersFooter.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
+    { path: "/", redirect: "/teams" },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      name: "teams",
+      path: "/teams",
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: "team-members",
+          path: ":teamId",
+          component: TeamMembers,
+          props: true,
+        }, // /teams/t1
+      ],
+    }, // our-domain.com/teams => TeamsList
+    {
+      path: "/users",
+      components: {
+        default: UsersList,
+        footer: UsersFooter,
+      },
+      beforeEnter(to, from, next) {
+        console.log("users beforeEnter");
+        console.log(to, from);
+        next();
+      },
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+  linkActiveClass: "active",
+  scrollBehavior(_, _2, savedPosition) {
+    // console.log(to, from, savedPosition);
+    if (savedPosition) {
+      return savedPosition;
     }
-  ]
-})
+    return { left: 0, top: 0 };
+  },
+});
 
-export default router
+router.beforeEach(function (to, from, next) {
+  console.log("Global beforeEach");
+  console.log(to, from);
+  if (to.meta.needsAuth) {
+    console.log("Needs auth!");
+    next();
+  } else {
+    next();
+  }
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  // next();
+});
+
+router.afterEach(function (to, from) {
+  // sending analytics data
+  console.log("Global afterEach");
+  console.log(to, from);
+});
+
+export default router;
