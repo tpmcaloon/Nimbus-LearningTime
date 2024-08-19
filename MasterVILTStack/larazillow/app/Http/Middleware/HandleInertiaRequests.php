29d0c12af581e46705948630c7a0f65420a22e99
Tom McAloon <tom.mcaloon@nimbushosting.co.uk>
2024-08-19 17:44:59 +0100

    Master VILT: Computed Data and Conditional Rendering

--- MasterVILTStack/larazillow/app/Http/Middleware/HandleInertiaRequests.php
+++ MasterVILTStack/larazillow/app/Http/Middleware/HandleInertiaRequests.php
@@ -11,7 +11,6 @@ class HandleInertiaRequests extends Middleware
      * The root template that's loaded on the first page visit.
      *
      * @see https://inertiajs.com/server-side-setup#root-template
-     *
      * @var string
      */
     protected $rootView = 'app';
@@ -20,6 +19,8 @@ class HandleInertiaRequests extends Middleware
      * Determines the current asset version.
      *
      * @see https://inertiajs.com/asset-versioning
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
      */
     public function version(Request $request): ?string
     {
@@ -27,16 +28,18 @@ class HandleInertiaRequests extends Middleware
     }
 
     /**
-     * Define the props that are shared by default.
+     * Defines the props that are shared by default.
      *
      * @see https://inertiajs.com/shared-data
-     *
-     * @return array<string, mixed>
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
      */
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'flash' => [
+                'success' => $request->session()->get('success')
+            ]
         ]);
     }
 }
--- MasterVILTStack/larazillow/resources/js/Layouts/MainLayout.vue
+++ MasterVILTStack/larazillow/resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,29 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>The page with time {{ timer }}</div>
+    <!-- <div>The page with time {{ timer }}</div> -->
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+    <div>{{ y }}</div>
     <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
-const timer = ref(0);
-setInterval(() => timer.value++, 1000);
+import { computed } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
+// const x = ref(0)
+// const y = computed(() => x.value * 2)
+const page = usePage();
+const flashSuccess = computed(() => page.props.flash.success);
+// import { ref } from 'vue'
+// const timer = ref(0)
+// setInterval(() => timer.value++, 1000)
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
