add47d59329c2bef4ebe4d542176bdfa422d0eec
Tom McAloon <tom.mcaloon@nimbushosting.co.uk>
2024-08-19 18:35:48 +0100

    Master VILT: Enhanced Styling on Listing Components

--- MasterVILTStack/larazillow/resources/js/Pages/Listing/Index.vue
+++ MasterVILTStack/larazillow/resources/js/Pages/Listing/Index.vue
@@ -1,29 +1,35 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <div>
-            <Link :href="route('listing.show', { listing: listing.id })">
-                <ListingAddress :listing="listing" />
-            </Link>
-        </div>
-        <div>
-            <Link :href="route('listing.edit', { listing: listing.id })"
-                >Edit</Link
-            >
-        </div>
-        <div>
-            <Link
-                :href="route('listing.destroy', { listing: listing.id })"
-                method="DELETE"
-                as="button"
-                >Delete</Link
-            >
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show', { listing: listing.id })">
+                    <ListingSpace :listing="listing" class="text-lg" />
+                    <ListingAddress :listing="listing" class="text-gray-500" />
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit', { listing: listing.id })">
+                    Edit
+                </Link>
+            </div>
+            <div>
+                <Link
+                    :href="route('listing.destroy', { listing: listing.id })"
+                    method="DELETE"
+                    as="button"
+                >
+                    Delete
+                </Link>
+            </div>
+        </Box>
     </div>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import ListingAddress from "@/Components/ListingAddress.vue";
+import Box from "@/Components/UI/Box.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
 defineProps({
     listings: Array,
 });
