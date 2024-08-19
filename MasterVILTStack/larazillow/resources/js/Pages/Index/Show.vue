373270831f311f79279b03191e9f5f175091cecf
Tom McAloon <tom.mcaloon@nimbushosting.co.uk>
2024-08-19 15:33:01 +0100

    Master VILT: Your First SPA / 006-default-layout

--- MasterVILTStack/larazillow/resources/js/Pages/Index/Show.vue
+++ MasterVILTStack/larazillow/resources/js/Pages/Index/Show.vue
@@ -2,14 +2,6 @@
     <div>Show</div>
     <Link href="/">Main Page</Link>
 </template>
-
 <script setup>
 import { Link } from "@inertiajs/vue3";
-</script>
-
-<script>
-import MainLayout from "../../Layouts/MainLayout.vue";
-export default {
-    layout: MainLayout,
-};
 </script>
