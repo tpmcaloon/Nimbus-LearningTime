f4dfd5ba0012219849e3b17ece7830ad9d0ba46b
Tom McAloon <tom.mcaloon@nimbushosting.co.uk>
2024-08-19 17:48:20 +0100

    Master VILT: Data Validation (Back End & Front End)

--- MasterVILTStack/larazillow/app/Http/Controllers/ListingController.php
+++ MasterVILTStack/larazillow/app/Http/Controllers/ListingController.php
@@ -9,6 +9,8 @@ class ListingController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -22,18 +24,33 @@ class ListingController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         return inertia('Listing/Create');
     }
-
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        Listing::create($request->all());
+        Listing::create(
+            $request->validate([
+                'beds' => 'required|integer|min:0|max:20',
+                'baths' => 'required|integer|min:0|max:20',
+                'area' => 'required|integer|min:15|max:1500',
+                'city' => 'required',
+                'code' => 'required',
+                'street' => 'required',
+                'street_nr' => 'required|min:1|max:1000',
+                'price' => 'required|integer|min:1|max:20000000',
+            ])
+        );
 
         return redirect()->route('listing.index')
             ->with('success', 'Listing was created!');
@@ -41,37 +58,50 @@ class ListingController extends Controller
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
     public function show(Listing $listing)
-{
-    return inertia(
-        'Listing/Show',
-        [
-            'listing' => $listing
-        ]
-    );
-}
+    {
+        return inertia(
+            'Listing/Show',
+            [
+                'listing' => $listing
+            ]
+        );
+    }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function edit(string $id)
+    public function edit($id)
     {
         //
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
         //
     }
-
+    
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
         //
     }
