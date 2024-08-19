aa2138fac15843ad4f7f7212bf1b3d92170569bb
Tom McAloon <tom.mcaloon@nimbushosting.co.uk>
2024-08-19 17:53:24 +0100

    Master VILT: Edit Listing Form

--- MasterVILTStack/larazillow/resources/js/Layouts/MainLayout.vue
+++ MasterVILTStack/larazillow/resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,10 @@
 <template>
-    <Link href="/">Main Page</Link>&nbsp;
-    <Link href="/hello">Show Page</Link>
+    <Link href="/listing">Listings</Link>&nbsp;
+    <Link href="/listing/create">New Listing</Link>
     <!-- <div>The page with time {{ timer }}</div> -->
     <div v-if="flashSuccess" class="success">
         {{ flashSuccess }}
     </div>
-    <div>{{ y }}</div>
     <slot>Default</slot>
 </template>
 
@@ -20,7 +19,6 @@ const flashSuccess = computed(() => page.props.flash.success);
 // const timer = ref(0)
 // setInterval(() => timer.value++, 1000)
 </script>
-
 <style scoped>
 .success {
     background-color: green;
