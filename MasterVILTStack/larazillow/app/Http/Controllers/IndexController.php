4f20ee66916dcc18fbfd982f08d13adace6f58a7
Tom McAloon <tom.mcaloon@nimbushosting.co.uk>
2024-08-19 19:48:34 +0100

    Master VILT: Login Logic

--- MasterVILTStack/larazillow/app/Http/Controllers/IndexController.php
+++ MasterVILTStack/larazillow/app/Http/Controllers/IndexController.php
@@ -1,6 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
+
+use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class IndexController extends Controller
 {
     public function index()
--- MasterVILTStack/larazillow/resources/js/Layouts/MainLayout.vue
+++ MasterVILTStack/larazillow/resources/js/Layouts/MainLayout.vue
@@ -13,15 +13,15 @@
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
                 <div>
-                    <Link :href="route('listing.create')" class="btn-primary">
-                        + New Listing
-                    </Link>
+                    <Link :href="route('listing.create')" class="btn-primary"
+                        >+ New Listing</Link
+                    >
                 </div>
             </nav>
         </div>
     </header>
 
-    <main class="container mx-auto p-4">
+    <main class="container mx-auto p-4 w-full">
         <div
             v-if="flashSuccess"
             class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2"
