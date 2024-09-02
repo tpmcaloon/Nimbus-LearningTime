5aedb1774acfbd300336dd26630c365dd2fa5600
Tom McAloon <tom.mcaloon@nimbushosting.co.uk>
2024-09-02 17:10:38 +0100

    Master VILT: One-To-Many DB Relationship

--- MasterVILTStack/larazillow/database/seeders/DatabaseSeeder.php
+++ MasterVILTStack/larazillow/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-        Listing::factory(20)->create();
+        Listing::factory(20)->create([
+            'by_user_id' => 1
+        ]);
     }
 }
