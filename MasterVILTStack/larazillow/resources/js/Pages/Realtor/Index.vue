923e08c5849cc6a139c6af4ec81ca57995fd05f1
Tom McAloon <tom.mcaloon@worldhost.group>
2024-09-30 13:32:31 +0100

    Master VILT: Realtor -> Show Listing that have been Soft Deleted

--- MasterVILTStack/larazillow/resources/js/Pages/Realtor/Index.vue
+++ MasterVILTStack/larazillow/resources/js/Pages/Realtor/Index.vue
@@ -1,6 +1,8 @@
 <template>
     <h1 class="text-3xl mb-4">Your Listings</h1>
-    <section class="mb-8">Filters</section>
+    <section>
+        <RealtorFilters />
+    </section>
     <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
         <Box v-for="listing in listings" :key="listing.id">
             <div
@@ -45,6 +47,7 @@ import ListingAddress from "@/Components/ListingAddress.vue";
 import ListingSpace from "@/Components/ListingSpace.vue";
 import Price from "@/Components/Price.vue";
 import Box from "@/Components/UI/Box.vue";
+import RealtorFilters from "@/Pages/Realtor/Index/Components/RealtorFilters.vue";
 import { Link } from "@inertiajs/vue3";
 defineProps({ listings: Array });
 </script>
