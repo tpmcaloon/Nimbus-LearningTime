028ba21889a493e44d6d406c34ef3ec398ba2411
Tom McAloon <tom.mcaloon@worldhost.group>
2024-09-30 13:50:35 +0100

    Master VILT: Realtor Filters (Default Values)

--- MasterVILTStack/larazillow/app/Http/Controllers/RealtorListingController.php
+++ MasterVILTStack/larazillow/app/Http/Controllers/RealtorListingController.php
@@ -23,9 +23,9 @@ class RealtorListingController extends Controller
         return inertia(
             'Realtor/Index',
             [
+                'filters' => $filters,
                 'listings' => Auth::user()
                     ->listings()
-                    // ->mostRecent()
                     ->filter($filters)
                     ->get()
             ]
--- MasterVILTStack/larazillow/resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ MasterVILTStack/larazillow/resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -1,6 +1,6 @@
 <template>
     <form>
-        <div class="mb-4 mt-4 flex flex-wrap gap-2">
+        <div class="mb-4 mt-4 flex flex-wrap gap-4">
             <div class="flex flex-nowrap items-center gap-2">
                 <input
                     id="deleted"
@@ -10,18 +10,62 @@
                 />
                 <label for="deleted">Deleted</label>
             </div>
+
+            <div>
+                <select v-model="filterForm.by" class="input-filter-l w-24">
+                    <option value="created_at">Added</option>
+                    <option value="price">Price</option>
+                </select>
+                <select v-model="filterForm.order" class="input-filter-r w-32">
+                    <option
+                        v-for="option in sortOptions"
+                        :key="option.value"
+                        :value="option.value"
+                    >
+                        {{ option.label }}
+                    </option>
+                </select>
+            </div>
         </div>
     </form>
 </template>
 
 <script setup>
-import { reactive, watch } from "vue";
+import { reactive, watch, computed } from "vue";
 import { router } from "@inertiajs/vue3";
 
-const filterForm = reactive({
-    deleted: false,
-});
+const sortLabels = {
+    created_at: [
+        {
+            label: "Latest",
+            value: "desc",
+        },
+        {
+            label: "Oldest",
+            value: "asc",
+        },
+    ],
+    price: [
+        {
+            label: "Pricey",
+            value: "desc",
+        },
+        {
+            label: "Cheapest",
+            value: "asc",
+        },
+    ],
+};
 
+const sortOptions = computed(() => sortLabels[filterForm.by]);
+const props = defineProps({
+    filters: Object,
+});
+const filterForm = reactive({
+    deleted: props.filters.deleted ?? false,
+    by: props.filters.by ?? "created_at",
+    order: props.filters.order ?? "desc",
+});
 watch(filterForm, () =>
     router.get(route("realtor.listing.index"), filterForm, {
         preserveState: true,
