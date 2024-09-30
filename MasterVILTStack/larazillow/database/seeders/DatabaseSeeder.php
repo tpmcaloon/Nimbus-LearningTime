dd9f4c99235bf8f48b9bc620a15bd64204a3448a
Tom McAloon <tom.mcaloon@worldhost.group>
2024-09-30 12:22:16 +0100

    Master VILT: Admin Override

--- MasterVILTStack/larazillow/database/seeders/DatabaseSeeder.php
+++ MasterVILTStack/larazillow/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Listing;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -10,21 +11,27 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'is_admin' => true
         ]);
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test2@example.com',
         ]);
-        Listing::factory(20)->create([
+        Listing::factory(10)->create([
             'by_user_id' => 1
+        ]);
+        Listing::factory(10)->create([
+            'by_user_id' => 2
         ]);
     }
 }
